8f70027132f8f6b8c4ed506363cbabbe8667bfce
Doris Morales <dmorales55@gmail.com>
2020-11-12 13:07:08 -0500

    Plantillas con Blade

--- tests/Feature/UsersModuleTest.php
+++ tests/Feature/UsersModuleTest.php
@@ -9,30 +9,38 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 class UsersModuleTest extends TestCase
 {
   /** @test */
-  function it_loads_the_users_list_page()
+  function it_shows_the_users_list()
   {
     $this->get('/usuarios')
-        ->assertStatus(200)
-        ->assertSee('Listado de usuarios')
-        ->assertSee('Joel')
-        ->assertSee('Ellie');
+      ->assertStatus(200)
+      ->assertSee('Listado de usuarios')
+      ->assertSee('Joel')
+      ->assertSee('Ellie');
   }
-    
+
+  /** @test */
+  function it_shows_a_default_message_if_the_users_list_is_empty()
+  {
+    $this->get('/usuarios?empty')
+      ->assertStatus(200)
+      ->assertSee('No hay usuarios registrados.');
+  }
+  
   /** @test */
   function it_loads_the_users_details_page()
   {
-    $this->get('/usuarios/5')
-        ->assertStatus(200)
-        ->assertSee('Mostrando detalle del usuario: 5');
+  $this->get('/usuarios/5')
+    ->assertStatus(200)
+    ->assertSee('Mostrando detalle del usuario: 5');
   }
   
   /** @test */
   function it_loads_the_new_users_page()
   {
-    $this->withoutExceptionHandling();
-    
-    $this->get('/usuarios/nuevo')
-        ->assertStatus(200)
-        ->assertSee('Crear nuevo usuario');
+  $this->withoutExceptionHandling();
+  
+  $this->get('/usuarios/nuevo')
+    ->assertStatus(200)
+    ->assertSee('Crear nuevo usuario');
   }
 }
