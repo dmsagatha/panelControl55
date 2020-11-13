1215762d42bbef2f8ebf07277deb103fe29a25f6
Doris Morales <dmorales55@gmail.com>
2020-11-12 19:58:41 -0500

    Usuarios: Listar

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -3,11 +3,11 @@
 @section('title', 'Usuarios')
 
 @section('content')
-  <h1>{{ $title }}</h1>
+  <h1 class="mt-3">{{ $title }}</h1>
 
   <ul>
     @forelse ($users as $user)
-      <li>{{ $user }}</li>
+      <li>{{ $user->id }} - {{ $user->name }} ({{ $user->email }})</li>
     @empty
       <li>No hay usuarios registrados.</li>
     @endforelse
@@ -16,6 +16,4 @@
 
 @section('sidebar')
   @parent
-
-  <h2>Barra lateral personalizada!</h2>
 @endsection
