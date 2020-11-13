d0024d614e82c443c5136eb4eefb2cc8761c6100
Doris Morales <dmorales55@gmail.com>
2020-11-13 18:30:03 -0500

    Adicionar estilos de Bootstrap al formulario

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -3,53 +3,53 @@
 @section('title', 'Crear usuario')
 
 @section('content')
-  <h1 class="mt-3">Crear usuario</h1>
+  <div class="card">
+    <div class="card-body">
+      <h1 class="card-title">Crear usuario</h1>
+      @if ($errors->any())
+        <div class="alert alert-danger">
+          <h6>Por favor corregir los errores:</h6>
+          <ul>
+            @foreach ($errors->all() as $error)
+              <li>{{ $error }}</li>
+            @endforeach
+          </ul>
+        </div>
+      @endif
 
-  @if ($errors->any())
-    <div class="alert alert-danger">
-      <h6>Por favor corregir los errores:</h6>
-      <ul>
-        @foreach ($errors->all() as $error)
-          <li>{{ $error }}</li>
-        @endforeach
-      </ul>
-    </div>
-  @endif
-
-  <form method="POST" action="{{ route('users.store') }}">
-    @csrf
-
-    <div class="form-row">
-      <div class="form-group col-4">
-        <label for="name">Nombre:</label>
-        <input class="form-control" type="text" name="name" id="name" value="{{ old('name') }}">
-        @if ($errors->has('name'))
-          <div class="text-danger">{{ $errors->first('name') }}</div>
-        @endif
-      </div>
-      <div class="form-group col">
-        <label for="email">Correo electrónico:</label>
-        <input class="form-control" type="email" name="email" id="email" value="{{ old('email') }}">
-        {!! $errors->first('email', '<div class="text-danger">:message</div>') !!}
-      </div>
-    </div>
-
-    <div class="form-row">
-      <div class="form-group col">
-        <label for="password">Contraseña:</label>
-        <input class="form-control" type="password" name="password" id="password" placeholder="Mayor a 6 caracteres">
-        {!! $errors->first('password', '<div class="text-danger">:message</div>') !!}
-      </div>
-    </div>
+      <form method="POST" action="{{ route('users.store') }}">
+        @csrf
     
-    <div class="card-header white white-text text-center btn-group-xs">
-    <button type="submit" class="btn btn-primary text-center">Crear usuario</button>
+        <div class="form-row">
+          <div class="form-group col-4">
+            <label for="name">Nombre:</label>
+            <input class="form-control" type="text" name="name" id="name" value="{{ old('name') }}">
+            @if ($errors->has('name'))
+              <div class="text-danger">{{ $errors->first('name') }}</div>
+            @endif
+          </div>
+          <div class="form-group col">
+            <label for="email">Correo electrónico:</label>
+            <input class="form-control" type="email" name="email" id="email" value="{{ old('email') }}">
+            {!! $errors->first('email', '<div class="text-danger">:message</div>') !!}
+          </div>
+        </div>
+    
+        <div class="form-row">
+          <div class="form-group col">
+            <label for="password">Contraseña:</label>
+            <input class="form-control" type="password" name="password" id="password" placeholder="Mayor a 6 caracteres">
+            {!! $errors->first('password', '<div class="text-danger">:message</div>') !!}
+          </div>
+        </div>
+        
+        <div class="card-header white white-text text-center btn-group-xs">
+          <button type="submit" class="btn btn-primary text-center">Crear usuario</button>
+          <a href="{{ route('users.index') }}" class="card-link">
+            Regresar al listado de usuarios
+          </a>
+        </div>
+      </form>      
     </div>
-  </form>
-
-  <hr>
-  
-  <p>
-    <a href="{{ route('users.index') }}">Regresar al listado de usuarios</a>
-  </p>
+  </div>
 @endsection
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -3,12 +3,16 @@
 @section('title', "Usuario {$user->name}")
 
 @section('content')
-  <h1 class="mt-3">Usuario #{{ $user->id }}</h1>
-  
-  <p>Nombre del usuario: {{ $user->name }}</p>
-  <p>Correo electrónico: {{ $user->email }}</p>
+  <div class="card">
+    <div class="card-body">
+      <h1 class="mt-3">Usuario #{{ $user->id }}</h1>
+    
+      <p>Nombre del usuario: {{ $user->name }}</p>
+      <p>Correo electrónico: {{ $user->email }}</p>
 
-  <p>
-    <a href="{{ route('users.index') }}">Regresar al listado de usuarios</a>
-  </p>
+      <p>
+        <a href="{{ route('users.index') }}">Regresar al listado de usuarios</a>
+      </p>
+    </div>
+  </div>
 @endsection
