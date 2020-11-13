f6efa4f1868e74717fa51cc26fd637db4478b99a
Doris Morales <dmorales55@gmail.com>
2020-11-13 10:29:27 -0500

    Enlace de modelos a rutas

--- routes/web.php
+++ routes/web.php
@@ -28,10 +28,10 @@ Route::get('/', function () {
 
 Route::get('/usuarios', 'UserController@index')->name('users.index');
 
-Route::get('/usuarios/{id}', 'UserController@show')
-    ->where('id', '[0-9]+')
+Route::get('/usuarios/{user}', 'UserController@show')
+    ->where('user', '[0-9]+')
     ->name('users.show');
 
-Route::get('/usuarios/nuevo', 'UserController@create');
+Route::get('/usuarios/nuevo', 'UserController@create')->name('users.create');
 
 Route::get('/saludo/{name}/{nickname?}', 'WelcomeUserController');
