f49005aa82a6c296751a0f022d7d5cabc0be8dec
Doris Morales <dmorales55@gmail.com>
2020-11-14 11:33:26 -0500

    Actualizado a Laravel 5.8

--- database/migrations/2020_11_12_140106_create_professions_table.php
+++ database/migrations/2020_11_12_140106_create_professions_table.php
@@ -9,7 +9,7 @@ class CreateProfessionsTable extends Migration
   public function up()
   {
     Schema::create('professions', function (Blueprint $table) {
-      $table->increments('id');
+      $table->bigIncrements('id');
       $table->string('title', 100)->unique();
       $table->timestamps();
     });
