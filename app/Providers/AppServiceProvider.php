8216cffbf3371fc10061ab76bb5da6c870b94b29
Doris Morales <dmorales55@gmail.com>
2020-11-16 09:41:18 -0500

    Comparti HTML en las vistas - componentes Blade

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 
@@ -18,6 +19,8 @@ class AppServiceProvider extends ServiceProvider
       'create' => 'crear',
       'edit'   => 'editar',
     ]);
+
+    Blade::component('shared._card', 'card');
   }
 
   /**
--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -3,22 +3,20 @@
 @section('title', 'Crear usuario')
 
 @section('content')
-  <div class="card">
-    <div class="card-body">
-      <h1 class="card-title">Crear usuario</h1>
-      
-      @include('shared._errors')
+  @component('shared._card')
+    @slot('header', 'Crear usuario')
+    
+    @include('shared._errors')
 
-      <form method="POST" action="{{ route('users.store') }}">
-        @include('users._fields')
-        
-        <div class="card-header white white-text text-center btn-group-xs">
-          <button type="submit" class="btn btn-primary text-center">Crear usuario</button>
-          <a href="{{ route('users.index') }}" class="card-link">
-            Regresar al listado de usuarios
-          </a>
-        </div>
-      </form>      
-    </div>
-  </div>
+    <form method="POST" action="{{ route('users.store') }}">
+      @include('users._fields')
+      
+      <div class="card-header white white-text text-center btn-group-xs">
+        <button type="submit" class="btn btn-primary text-center">Crear usuario</button>
+        <a href="{{ route('users.index') }}" class="card-link">
+          Regresar al listado de usuarios
+        </a>
+      </div>
+    </form>
+  @endcomponent
 @endsection
