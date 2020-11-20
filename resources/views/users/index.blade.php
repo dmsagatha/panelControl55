63ae4960f3f4a12276af348d2e61f3af0ef23a33
Doris Morales <dmorales55@gmail.com>
2020-11-20 14:44:19 -0500

    Consultar relaciones existentes usando has y doesntHave de Eloquent

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -17,14 +17,15 @@
   @if ($users->isNotEmpty())    
     <div class="table-responsive-lg">
       <table class="table table-sm">
-        <thead class="thead-dark">
+        <thead class="thead-dark text-center">
           <tr>
             <th scope="col"># <span class="oi oi-caret-bottom"></span><span class="oi oi-caret-top"></span></th>
             <th scope="col" class="sort-desc">Nombre <span class="oi oi-caret-bottom"></span><span class="oi oi-caret-top"></span></th>
+            <th scope="col">Empresa <span class="oi oi-caret-bottom"></span><span class="oi oi-caret-top"></span></th>
             <th scope="col">Correo <span class="oi oi-caret-bottom"></span><span class="oi oi-caret-top"></span></th>
             <th scope="col">Rol <span class="oi oi-caret-bottom"></span><span class="oi oi-caret-top"></span></th>
-            <th scope="col">Fechas <span class="oi oi-caret-bottom"></span><span class="oi oi-caret-top"></span></th>
-            <th scope="col" class="text-right th-actions">Acciones</th>
+            {{-- <th scope="col">Fechas <span class="oi oi-caret-bottom"></span><span class="oi oi-caret-top"></span></th> --}}
+            <th scope="col" class="th-actions">Acciones</th>
           </tr>
         </thead>
         <tbody>
