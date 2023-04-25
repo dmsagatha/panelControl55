95557879dc05ddd52ca2a108c72e71119697a243
Doris Morales <dmorales55@gmail.com>
2023-04-25 15:11:58 -0500

    Actualizacion a Laravel 10

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -14,11 +14,13 @@ class Kernel extends HttpKernel
    * @var array
    */
   protected $middleware = [
-    \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
+    // \App\Http\Middleware\TrustHosts::class,
+    \App\Http\Middleware\TrustProxies::class,
+    \Illuminate\Http\Middleware\HandleCors::class,
     \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
     \App\Http\Middleware\TrimStrings::class,
     \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-    \App\Http\Middleware\TrustProxies::class,
+    \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
   ];
 
   /**
@@ -31,15 +33,15 @@ class Kernel extends HttpKernel
       \App\Http\Middleware\EncryptCookies::class,
       \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
       \Illuminate\Session\Middleware\StartSession::class,
-      // \Illuminate\Session\Middleware\AuthenticateSession::class,
       \Illuminate\View\Middleware\ShareErrorsFromSession::class,
       \App\Http\Middleware\VerifyCsrfToken::class,
       \Illuminate\Routing\Middleware\SubstituteBindings::class,
     ],
 
     'api' => [
-      'throttle:api',
-      'bindings',
+      // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+      \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
+      \Illuminate\Routing\Middleware\SubstituteBindings::class,
     ],
   ];
 
@@ -50,12 +52,16 @@ class Kernel extends HttpKernel
    *
    * @var array
    */
-  protected $routeMiddleware = [
-    'auth' => \Illuminate\Auth\Middleware\Authenticate::class,
-    'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
-    'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
-    'can' => \Illuminate\Auth\Middleware\Authorize::class,
-    'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
-    'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+    protected $middlewareAliases = [
+      'auth' => \App\Http\Middleware\Authenticate::class,
+      'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
+      'auth.session' => \Illuminate\Session\Middleware\AuthenticateSession::class,
+      'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
+      'can' => \Illuminate\Auth\Middleware\Authorize::class,
+      'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
+      'password.confirm' => \Illuminate\Auth\Middleware\RequirePassword::class,
+      'signed' => \App\Http\Middleware\ValidateSignature::class,
+      'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+      'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
   ];
 }
